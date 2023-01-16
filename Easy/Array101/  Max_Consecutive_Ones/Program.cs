afee333b98996cdbeed1755844b76e3daf62b172
AibekUsubaliev <aibechelllo@gmail.com>
2023-01-16 20:33:22 +0600

    first commit for tracking my person progress

--- Easy/Array101/  Max_Consecutive_Ones/Program.cs
+++ Easy/Array101/  Max_Consecutive_Ones/Program.cs
@@ -1,7 +1,9 @@
 public class Solution {
     public int FindMaxConsecutiveOnes(int[] nums) {
         int maxValue = 0;
+
         int count = 0;
+
         for(int i = 0; i<nums.Length; i++)
         {
             if(nums[i] == 1){
@@ -12,6 +14,7 @@ public class Solution {
                 count = 0;
             }  
          }
+         
          return maxValue;
     }
 }
