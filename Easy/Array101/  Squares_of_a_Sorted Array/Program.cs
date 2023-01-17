6bbb073c4399bc8b8805b58d803f48c696f16b46
aibechelllo <iplaydota4sk@gmail.com>
2023-01-17 23:06:37 +0600

    Squares of a Sorted Array

---
+++ Easy/Array101/  Squares_of_a_Sorted Array/Program.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        int[] result = new int[nums.Length];
+        
+        for(int i = 0; i < nums.Length; i++){
+            result[i] = (int)Math.Pow(nums[i], 2);
+        }
+        Array.Sort(result);
+        return result;
+    }
+}
---
+++ Easy/Array101/  Squares_of_a_Sorted Array/README.md
@@ -0,0 +1,3 @@
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
